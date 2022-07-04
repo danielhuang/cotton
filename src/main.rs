03d5175e671a1ab909646cee3a7e9506476b4605
Dan <dan@yellowiki.xyz>
2022-07-04 17:28:17 -0400

    Revert "add: allow missing dependencies field"
    This reverts commit 33c39e5bdd50bf3f05b8ced0e1ef330c61d5f04a.

--- src/main.rs
+++ src/main.rs
@@ -204,7 +204,7 @@ async fn main() -> Result<()> {
                 .as_object_mut()
                 .wrap_err("`package.json` is invalid")?
                 .get_mut("dependencies")
-                .unwrap_or(&mut Value::Object(Default::default()))
+                .wrap_err("`package.json` is missing `dependencies`")?
                 .as_object_mut()
                 .wrap_err("`package.json` contains invalid `dependencies`")?
                 .insert(name.to_string(), Value::String(format!("^{}", latest)));
