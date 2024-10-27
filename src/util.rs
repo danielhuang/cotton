0b0152c010ad0178b8e9093314a98d68e8a0e85f
Daniel <hello@danielh.cc>
2024-10-27 14:49:19 -0400

    tweak wrap_err

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Context, Result};
 use color_eyre::Report;
 use compact_str::{CompactString, ToCompactString};
 use node_semver::{Range, Version};
@@ -144,7 +144,7 @@ pub async fn retry<T, Fut: Future<Output = Result<T>>>(mut f: impl FnMut() -> Fu
             }
         }
     }
-    Err(last.unwrap().wrap_err("Failed all retries"))
+    Err(last.unwrap()).wrap_err("Failed all retries")
 }
 
 pub async fn read_package() -> Result<Package> {
